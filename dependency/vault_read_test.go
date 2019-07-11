a800b34a04275a4d7cc9763a1ffc3e93c148eefe
John Eikenberry <jae@zhar.net>
2019-07-10 17:05:46 -0700

    tests now use an external vault process
    Replace use of core vault package for testing with an external vault
    process. Previously each set of tests that needed vault would create a
    vault service in the test process and all tests would use the default
    'secret' kv path. Now it uses a separate kv secrets path for each set of
    tests, setting the version for each.
    
    The code for starting up the Vault process was based off what was
    already used for Consul via its sdk/testutils package.
    
    In the process various test conflicts were addressed and an external
    process (consul and vault) process leak was fixed [1].
    
    [1] the leak was caused by use of log.Fatal which calls os.Exit which
    exist the process without running any cleanup code.

--- dependency/vault_read_test.go
+++ dependency/vault_read_test.go
@@ -81,11 +81,20 @@ func TestNewVaultReadQuery(t *testing.T) {
 	}
 }
 
-func TestVaultReadQuery_Fetch(t *testing.T) {
+func TestVaultReadQuery_Fetch_KVv1(t *testing.T) {
 	t.Parallel()
 
-	clients, vault := testVaultServer(t)
-	defer vault.Stop()
+	clients, vault := testVaultServer("read_fetch_v1", "1")
+	secrets_path := vault.secrets_path
+	// Enable v1 kv for versioned secrets
+	vc := clients.Vault()
+	if err := vc.Sys().TuneMount(secrets_path, api.MountConfigInput{
+		Options: map[string]string{
+			"version": "1",
+		},
+	}); err != nil {
+		t.Fatalf("Error tuning secrets engine: %s", err)
+	}
 
 	err := vault.CreateSecret("foo/bar", map[string]interface{}{
 		"ttl": "100ms", // explicitly make this a short duration for testing
@@ -103,7 +112,7 @@ func TestVaultReadQuery_Fetch(t *testing.T) {
 	}{
 		{
 			"exists",
-			"secret/foo/bar",
+			secrets_path + "/foo/bar",
 			&Secret{
 				Data: map[string]interface{}{
 					"ttl": "100ms",
@@ -144,7 +153,7 @@ func TestVaultReadQuery_Fetch(t *testing.T) {
 	}
 
 	t.Run("stops", func(t *testing.T) {
-		d, err := NewVaultReadQuery("secret/foo/bar")
+		d, err := NewVaultReadQuery(secrets_path + "/foo/bar")
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -181,7 +190,7 @@ func TestVaultReadQuery_Fetch(t *testing.T) {
 	})
 
 	t.Run("fires_changes", func(t *testing.T) {
-		d, err := NewVaultReadQuery("secret/foo/bar")
+		d, err := NewVaultReadQuery(secrets_path + "/foo/bar")
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -216,18 +225,8 @@ func TestVaultReadQuery_Fetch(t *testing.T) {
 func TestVaultReadQuery_Fetch_KVv2(t *testing.T) {
 	t.Parallel()
 
-	clients, vault := testVaultServer(t)
-	defer vault.Stop()
-
-	// Enable v2 kv for versioned secrets
-	vc := clients.Vault()
-	if err := vc.Sys().TuneMount("secret", api.MountConfigInput{
-		Options: map[string]string{
-			"version": "2",
-		},
-	}); err != nil {
-		t.Fatalf("Error tuning secrets engine: %s", err)
-	}
+	clients, vault := testVaultServer("read_fetch_v2", "2")
+	secrets_path := vault.secrets_path
 
 	// Write an initial value to the secret path
 	err := vault.CreateSecret("data/foo/bar", map[string]interface{}{
@@ -259,7 +258,7 @@ func TestVaultReadQuery_Fetch_KVv2(t *testing.T) {
 	}{
 		{
 			"exists",
-			"secret/foo/bar",
+			secrets_path + "/foo/bar",
 			&Secret{
 				Data: map[string]interface{}{
 					"data": map[string]interface{}{
@@ -272,7 +271,7 @@ func TestVaultReadQuery_Fetch_KVv2(t *testing.T) {
 		},
 		{
 			"/data in path",
-			"secret/data/foo/bar",
+			secrets_path + "/data/foo/bar",
 			&Secret{
 				Data: map[string]interface{}{
 					"data": map[string]interface{}{
@@ -285,7 +284,7 @@ func TestVaultReadQuery_Fetch_KVv2(t *testing.T) {
 		},
 		{
 			"version=1",
-			"secret/foo/bar?version=1",
+			secrets_path + "/foo/bar?version=1",
 			&Secret{
 				Data: map[string]interface{}{
 					"data": map[string]interface{}{
@@ -329,7 +328,7 @@ func TestVaultReadQuery_Fetch_KVv2(t *testing.T) {
 	}
 
 	t.Run("stops", func(t *testing.T) {
-		d, err := NewVaultReadQuery("secret/foo/bar")
+		d, err := NewVaultReadQuery(secrets_path + "/foo/bar")
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -367,7 +366,8 @@ func TestVaultReadQuery_Fetch_KVv2(t *testing.T) {
 
 	for _, dataPrefix := range []string{"", "/data"} {
 		t.Run(fmt.Sprintf("fires_changes%s", dataPrefix), func(t *testing.T) {
-			d, err := NewVaultReadQuery(fmt.Sprintf("secret%s/foo/bar", dataPrefix))
+			d, err := NewVaultReadQuery(fmt.Sprintf("%s%s/foo/bar",
+				secrets_path, dataPrefix))
 			if err != nil {
 				t.Fatal(err)
 			}
